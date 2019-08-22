fba4e2b0faeb84fe9f264da7b1ef02af7c423408
lezlinp <lezlinp@gmail.com>
2019-08-22 15:49:03 +1200

    hello from Lin

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div>
-      Hello, From Tatsuki, Ryan, Christian
+      Hello, From Tatsuki, Ryan, Christian, Lin
     </div>
   );
 }
