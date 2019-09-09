38bbcb79ffa10e5b3e603a764b610c7bfc0e2ce4
temaari <53286793+temaari@users.noreply.github.com>
2019-09-09 12:08:19 +1200

    added more funcionality

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -1,12 +1,20 @@
 import React, { Props } from 'react';
 import ReactDOM from 'react-dom';
+import note from './note';
+import setText from './note';
 
-export default function dropDownList() {
-    return (
+export default function dropDownList() { 
+  return (
       <select>
-        <option>Date</option>
+        <option onClick={dateClick} >Date</option>
         <option>Time</option>
         <option>Favourites</option>
       </select>
+    );
+  }
+
+  function dateClick()  {
+    return (
+      setText()
     );
   }
--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,10 +1,19 @@
 import React, { Props } from 'react';
 import ReactDOM from 'react-dom';
 
+var said = "Hello there";
+
 export default function note()  {
     return (
       <button>
-        <textarea readOnly disabled>This is a test note</textarea>
+        <textarea readOnly disabled rows={4} cols={40} >{ said }</textarea>
       </button>
+    );
+  }
+
+  export function setText(props : any)  {
+    return (
+      said ="This is some text" + props
+      
     );
   }
