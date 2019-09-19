2cd261b6bbb21ea5ff7b4095f62b3747c6ad52d7
Duvvvy <ryanbircham@gmail.com>
2019-09-19 12:35:06 +1200

    Kinda works, need to fix CSS for map view as long notes overlap

--- src/Components/NotesView.tsx
+++ src/Components/NotesView.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Modal, ModalHeader, ModalBody} from 'reactstrap';
 
-const debugDBImages = [
+var items = [
   {
     title: "test note title 1",
+    body: "test body 1",
     description: "test note description 1",
     date: "1st of testuary",
     time: "11am",
@@ -12,7 +13,15 @@ const debugDBImages = [
 },
 {
     title: "test note title 2",
-    description: "test note description 2",
+    body: "test body 2",
+    description: `test note description 2, 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit rhoncus egestas. Donec sapien ante, aliquam cursus diam gravida, varius pharetra sapien. Donec non pharetra velit. Morbi nec suscipit orci. Nunc dui tortor, pulvinar sit amet mattis ac, aliquam ut dui. Donec aliquet ut turpis maximus mollis. Sed pellentesque ligula at neque rhoncus pretium. Donec tincidunt magna neque, imperdiet suscipit justo sodales ac.
+
+Aenean tempus, justo ac dictum mollis, urna nibh venenatis orci, non vulputate dolor massa a mauris. Morbi egestas interdum massa, ac vestibulum ante. Nullam id luctus mi. Nam cursus porttitor mauris. Cras a quam sit amet mi sagittis interdum eu nec odio. Integer ex odio, feugiat consectetur libero eu, auctor condimentum neque. In facilisis augue nec dui faucibus, a congue nulla iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque aliquam vestibulum ipsum. Nullam tempus viverra commodo. Maecenas aliquet nisl a odio bibendum, et pulvinar lorem fringilla. Curabitur vitae libero leo. Fusce facilisis tortor sed ullamcorper suscipit.
+
+Aenean sed leo cursus, ultrices ante id, molestie sem. Donec venenatis arcu sed lectus suscipit, nec aliquam dolor malesuada. Nam semper vitae felis a vestibulum. Quisque vitae facilisis felis, id tincidunt dolor. Vestibulum blandit, ipsum a egestas porta, lorem felis fermentum turpis, ultrices imperdiet eros mauris vitae mauris. Nunc vitae sem at leo elementum fermentum non nec ante. Morbi consequat quam urna, at eleifend arcu auctor quis.
+    
+    `,
     date: "2nd of testuary",
     time: "12am",
     noteID: "n2",
@@ -20,6 +29,7 @@ const debugDBImages = [
 },
 {
     title: "test note title 3",
+    body: "test body 3",
     description: "test note description 3",
     date: "3rd of testuary",
     time: "3pm",
@@ -28,6 +38,7 @@ const debugDBImages = [
 },
 {
     title: "test note title 4",
+    body: "test body 4",
     description: "test note description 4",
     date: "4th of testuary",
     time: "4pm",
@@ -41,18 +52,24 @@ interface IState{
   isModalOpen: boolean,
   title: String,
   description: String,
+  body: string,
   date: String,
   time: number,
   currentCard: number,
+  currentImageBase64: any,
+  ratio: number
 }
 
 const initialState = {
   isModalOpen: false,
   title: "no title",
   description: "no description",
+  body: "no body",
   date: "not long ago?",
   time: 0,
   currentCard: 0,
+  currentImageBase64: 0,
+  ratio: 1
 }
 
 interface IProps{
@@ -69,17 +86,22 @@ class NotesView extends React.Component <IProps, IState> {
   }
 
   async openImage(index:number) {
-    const image = debugDBImages[index];
+    console.log(index);
+    const image = items[index];
       const base_image = new Image();
       base_image.src = image.src;
       const base64 = await this.getBase64Image(image.src);
       const ratio = await (base_image.width / base_image.height);
-      this.setState(() => ({
-        currentImage: index,
+      this.setState({
+        currentCard: index,
         currentImageBase64: base64,
+        isModalOpen: true,
         ratio: ratio,
-        ...initialState
-      }), () => this.toggle());
+        title: items[index].title,
+        body: items[index].body,
+        description: items[index].description
+      })
+      console.log(this.state)
     }
 
     async getBase64Image(url: any) {
@@ -104,42 +126,31 @@ class NotesView extends React.Component <IProps, IState> {
 
 
 render() {
-    const textStyle = {
-      fontFamily: "helvetica",
-      fontSize: "50px",
-      textTransform: "none",
-      fill: "#FFF",
-      stroke: "#000",
-      userSelect: "none"
-    }
-
     return (
       <div>
         <div className="main-content">
           <div className="content">
-            {debugDBImages.map((image, index) => (
-              <div className="image-holder" key={image.src}>
-                <span 
-                  className="bottom-caption"
+            {items.map((textArea, index) => (
+              <div className="image-holder" key={textArea.title}>
+                <p >{items[index].date}</p>
+                <p>{items[index].description}</p>
+                <span className="bottom-caption"
                   onClick={() => this.openImage(index)}
-                >Test Note title</span>
-                <p 
-                  id='clickable test'
-                  
-                
-                
-                >test note body elemtns abcd test test test</p>
+                >
+                  {items[index].title}
+                </span>
+                <p id='note-title'>
+                </p>
               </div>
             ))}
           </div>
         </div>
         <Modal className="meme-modal" isOpen={this.state.isModalOpen} size="lg">
-          <ModalHeader toggle={this.toggle}>Make-a-Meme</ModalHeader>
-          <ModalBody>
-              <p id='note-body'>test note text</p>
-            <div className="meme-text-input">
-              <button onClick={() => console.log("button clicked")} className="btn btn-primary">Download</button>
-            </div>
+          <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
+          <ModalBody id='modal-body'>
+              <p id='note-body'>
+                {this.state.description}
+              </p>
           </ModalBody>
         </Modal>
       </div>
