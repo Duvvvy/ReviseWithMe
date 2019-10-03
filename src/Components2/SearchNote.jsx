e94328e0f065c49e1e0418f33db5c678b11bf6b6
temaari <53286793+temaari@users.noreply.github.com>
2019-10-03 14:44:16 +1300

    update search

--- src/Components2/SearchNote.jsx
+++ src/Components2/SearchNote.jsx
@@ -5,9 +5,11 @@ const details = [{}]
 
 function searchingFor(term) {
     return function (x) {
+        if (term !== null) {
         return (x.title.toLowerCase().includes(term.toLowerCase())
             || x.description.toLowerCase().includes(term.toLowerCase())
             || !term)
+        }
     }
 }
 
@@ -18,37 +20,23 @@ export class SearchNote extends React.Component {
 
         this.state = {
             details: items,
-            term: ' Search notes...'
+            term: null
         }
         this.searchHandler = this.searchHandler.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event)  {
-        this.setState({
-            term:' '
-        })
     }
 
     searchHandler(event) {
-        if (!event.target.value)    {
-            this.setState({
-                details: items,
-                term: ' Search notes...'
-            })
-        }
-        else{
         this.setState({
             details: items,
             term: event.target.value
-        })}
+        })
     }
 
     displayNote(term, person)   {
-        if (term !== " " )    {
+        if (term !== null || term == " ")    {
             return(
                 <div key={person.noteID}>
-                    <p>Look at note "{person.title}"</p>
+                    <p>Look at note "{person.title}":{term}</p>
                 </div>
                 )
         }
@@ -58,9 +46,8 @@ export class SearchNote extends React.Component {
         const { term, details } = this.state;
         return (
             <div>
-                <input id='Search-Notes'type="text"
+                <input id='Search-Notes'type="text" placeholder='Search Notes...'
                     onChange={this.searchHandler}
-                    onClick={this.handleClick}
                     value={term}
                 />
                 <div>
